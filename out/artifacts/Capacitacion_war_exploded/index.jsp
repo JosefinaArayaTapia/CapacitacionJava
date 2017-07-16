57f10af6c15a32b2a70ad3d05604c2df04507a75
Josefina Araya <araya.tapia.j@gmail.com>
2017-07-15 23:40:15 -0400

    Primer Ejemplo de Servlet

---
+++ out/artifacts/Capacitacion_war_exploded/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Josefina
+  Date: 15-07-17
+  Time: 23:07
+  To change this template use File | Settings | File Templates.
+--%>
+<html>
+<body>
+<form action="login" method="post">
+  <table>
+    <tr>
+      <td>User</td>
+      <td><input name="user" /></td>
+    </tr>
+    <tr>
+      <td>password</td>
+      <td><input name="password" /></td>
+    </tr>
+  </table>
+  <input type="submit" />
+</form>
+</body>
+</html>
---
+++ src/com/josefina/servlets/LoginServlet.java
@@ -0,0 +1,44 @@
+package com.josefina.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pass = req.getParameter("password");
+		if ("admin".equals(user) && "admin".equals(pass)) {
+			response(resp, "Login Ok");
+		} else {
+			response(resp, "Login Invalido");
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = req.getParameter("user");
+		String pass = req.getParameter("password");
+		if ("admin".equals(user) && "admin".equals(pass)) {
+			response(resp, "Login Ok");
+		} else {
+			response(resp, "Login Invalido");
+		}
+	}
+
+	private void response(HttpServletResponse resp, String msg)
+			throws IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>" + msg + "</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
